a55842541ba26e0eab39220052bee12d145fc589
Nicolas <nico.daddabbo7100@gmail.com>
2025-08-12 11:45:48 +0200

    server: changement de type pour reçvoir la conversation côté Gemini

--- MCP-Client/src/googleAI/gemini.ts
+++ MCP-Client/src/googleAI/gemini.ts
@@ -1,6 +1,7 @@
-import { GoogleGenAI, mcpToTool } from "@google/genai";
+import { ContentListUnion, GoogleGenAI, mcpToTool } from "@google/genai";
 import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import dotenv from "dotenv";
+import { Content } from "openai/resources/containers/files/content";
 dotenv.config();
 
 const ai = new GoogleGenAI({
@@ -8,7 +9,7 @@ const ai = new GoogleGenAI({
 });
 const model = "gemini-2.5-flash-lite";
 
-export async function callGemini(prompt: string, client: Client) {
+export async function callGemini(prompt: ContentListUnion, client: Client) {
   const response = await ai.models.generateContent({
     model,
     contents: prompt,
